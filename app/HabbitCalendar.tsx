37fe6b17287e2d8f3b8fa11ac95fe8a31aa110d2
murilooon <murilooon@gmail.com>
2021-01-07 13:18:38 -0300

    feat: add calendar and a simple async storage

--- app/HabbitCalendar.tsx
+++ app/HabbitCalendar.tsx
@@ -1,31 +1,72 @@
-import React from 'react';
-import { Calendar } from 'react-native-calendars';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useEffect } from 'react';
+import type { DateObject, PeriodMarking } from 'react-native-calendars';
+import { CalendarList } from 'react-native-calendars';
 
-export default function HabbitCalendar() {
+type DatesProps = {
+  [date: string]: PeriodMarking;
+};
+
+type HabbitCalendarProps = {
+  onChange: (day: DateObject) => void;
+  dates: { [date: string]: PeriodMarking };
+  setDates: (prevState: DatesProps) => void;
+};
+
+export default function HabbitCalendar({
+  onChange,
+  dates,
+  setDates,
+}: HabbitCalendarProps) {
+  const save = async (value: unknown) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem('@Dates', jsonValue);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const date = {
+    '2021-01-08': {
+      startingDay: true,
+      color: '#f9ca60',
+      endingDay: true,
+      textColor: 'white',
+    },
+    '2021-01-09': {
+      startingDay: true,
+      color: '#f9ca60',
+      endingDay: true,
+      textColor: 'white',
+    },
+  };
+
+  save(date);
+
+  useEffect(() => {
+    const load = async () => {
+      try {
+        const jsonValue = await AsyncStorage.getItem('@Dates');
+
+        setDates(jsonValue != null ? JSON.parse(jsonValue) : null);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    load();
+  }, [setDates]);
+
   return (
-    <Calendar
-      markedDates={{
-        '2021-01-05': {
-          startingDay: true,
-          color: '#94cf83',
-          textColor: 'white',
-        },
-        '2021-01-06': { color: '#94cf83', textColor: 'white' },
-        '2021-01-07': { color: '#94cf83', endingDay: true, textColor: 'white' },
-        '2021-01-08': {
-          startingDay: true,
-          color: '#f9ca60',
-          endingDay: true,
-          textColor: 'white',
-        },
-        '2021-01-09': {
-          startingDay: true,
-          color: '#f06f6a',
-          textColor: 'white',
-        },
-        '2021-01-10': { color: '#f06f6a', endingDay: true, textColor: 'white' },
-      }}
+    <CalendarList
+      markedDates={dates}
       markingType={'period'}
+      onDayPress={(day) => {
+        onChange(day);
+      }}
+      horizontal={true}
+      pagingEnabled={true}
     />
   );
 }
